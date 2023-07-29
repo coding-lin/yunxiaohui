4766fe7aaac2894c9bd6b660a647385c60a1b1e3
hjl <2764881569@qq.com>
2023-07-29 18:26:50 +0800

    feat:登录功能

--- pages/login/index.js
+++ pages/login/index.js
@@ -6,27 +6,47 @@ Page({
    */
   data: {
     stuId: '',
-    password: ''
+    password: '',
+    saveCount: true
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.initAccount()
+  },
 
+  /**
+   * 初始化账号
+   */
+  initAccount() {
+    const accountCache = wx.getStorageSync('account')
+    if (accountCache) {
+      this.setData({
+        ...accountCache
+      })
+    }
   },
   
+  /**
+   * 登录
+   */
   login() {
+    const that = this;
     const postData = {
-      stuId: this.data.stuId,
-      password: this.data.password
+      stuId: that.data.stuId,
+      password: that.data.password
     }
+    wx.showLoading({
+      title: '登录中',
+    })
     wx.request({
       url: 'http://127.0.0.1:3000/login',
       data: postData,
       method: 'POST',
       success(res) {
-        console.log(res)
+        wx.hideLoading()
         let data = res.data
         if (data.code === -1) {
           wx.showToast({
@@ -35,12 +55,28 @@ Page({
           })
           return
         }
-        wx.setStorage('token', data.data.cookie)
+        wx.setStorageSync('token', data.data.cookie)
+        if (that.data.saveCount) {
+          wx.setStorageSync('account', postData)
+        } else {
+          wx.removeStorageSync('account')
+        }
         wx.showToast({
           title: '登录成功',
           icon: 'none'
         })
+        setTimeout(() => {
+          wx.redirectTo({
+            url: '/pages/index/index',
+          })
+        }, 1000)
       }
+    })
+  },
+  
+  switchStatus() {
+    this.setData({
+      saveCount: !this.data.saveCount
     })
   },
 
