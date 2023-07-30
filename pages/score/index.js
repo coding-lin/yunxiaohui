7ada35acf6d835dcaac25efdeee7d134ac07189c
hjl <2764881569@qq.com>
2023-07-30 11:32:42 +0800

    feat:成绩查询功能

--- pages/score/index.js
+++ pages/score/index.js
@@ -1,6 +1,9 @@
 // pages/score/index.js
 import { getScoreListRequest, getRawScoreListRequest } from '../../api/index'
 
+const scoreCacheKey = "scores"
+const rawScoreCacheKey = "rawScores"
+
 Page({
 
   /**
@@ -8,7 +11,8 @@ Page({
    */
   data: {
     type: 1,  // 1为有效成绩，2为原始成绩
-    list: []
+    list: [],  // 成绩列表
+    termIndex: 0  // 当前学期索引
   },
 
   /**
@@ -19,18 +23,47 @@ Page({
   },
 
   getList() {
+    const cache = wx.getStorageSync(this.data.type == 1 ? scoreCacheKey : rawScoreCacheKey)
+    if (cache) {
+      this.setData({
+        list: cache
+      })
+      return
+    }
+    this.update()
+  },
+
+  // 刷新
+  update() {
     const that = this
     let p = null
-    if (that.data.type === 1) {
+    if (that.data.type == 1) {
       p = getScoreListRequest()
     } else {
       p = getRawScoreListRequest()
     }
     p.then(res => {
-      console.log(res)
       that.setData({
         list: res.data
       })
+      wx.setStorageSync(that.data.type == 1 ? scoreCacheKey : rawScoreCacheKey, res.data)
+    })
+  },
+
+  // 切换成绩类型
+  changeScoreType(e) {
+    const type = e.currentTarget.dataset.type
+    this.setData({
+      type
+    })
+    this.getList()
+  },
+  
+  // 切换学期
+  changeTerm(e) {
+    const termIndex = e.detail.value
+    this.setData({
+      termIndex
     })
   }
 })
