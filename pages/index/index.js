0df4fb23cd567b00e8d6771ef0d76969bbf489b1
hjl <2764881569@qq.com>
2023-07-30 18:36:25 +0800

    feat:课表查询功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,47 +2,68 @@
 // 获取应用实例
 const app = getApp()
 
+import { getNowWeek } from '../../utils/util'
+
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    navList: [{
+        title: '查课表',
+        icon: '/assets/images/course.png',
+        path: '/pages/course/index'
+      },
+      {
+        title: '查成绩',
+        icon: '/assets/images/score.png',
+        path: '/pages/score/index'
+      },
+      {
+        title: '查考勤',
+        icon: '/assets/images/attendance.png',
+        path: '/pages/attendance/index'
+      },
+      {
+        title: '校历',
+        icon: '/assets/images/calendar.png',
+        path: '/pages/calendar/index'
+      },
+    ],
+    startDate: '2023/02/20', // 开学日期
+    totalWeek: 20,
+    todayCourseList: []
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+    this.getTodayCourseList()
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+
+  nav(e) {
+    const index = e.currentTarget.dataset.index
+    const path = this.data.navList[index].path
+    wx.navigateTo({
+      url: path,
+      fail() {
+        wx.switchTab({
+          url: path,
         })
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+
+  getTodayCourseList() {
+    // const todayWeek = 2
+    // const todayWeeks = 15
+    const todayWeek = new Date().getDay()
+    const todayWeeks = getNowWeek(this.data.startDate, this.data.totalWeek)
+    const courseList = wx.getStorageSync('courses')
+    const todayCourseList = courseList.filter(item => {
+      return item.week == todayWeek && item.weeks.indexOf(todayWeeks) > -1
+    })
+    todayCourseList.sort((a, b) => {
+      return a.section - b.section
+    })
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      todayWeek,
+      todayWeeks,
+      todayCourseList
     })
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -14,6 +14,18 @@ const formatNumber = n => {
   return n[1] ? n : `0${n}`
 }
 
+function getNowWeek(startDate, totalWeek) {
+  const nowDate = new Date().getTime()
+  startDate = new Date(startDate)
+  const time = nowDate - startDate
+  let nowWeek = Math.ceil(time / 1000 / 60 / 60 / 24 / 7)
+  if (nowWeek > totalWeek) {
+    nowWeek = 1
+  }
+  return nowWeek
+}
+
 module.exports = {
-  formatTime
+  formatTime,
+  getNowWeek
 }
